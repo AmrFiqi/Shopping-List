893fdee70e65240f956589b8914f33400c90c4f8
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-01-09 15:23:36 +0200

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Amr El-Fiqi on 09/01/2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
